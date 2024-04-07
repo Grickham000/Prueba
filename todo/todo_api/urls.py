4f30bdf63fcfe3408503e90ab7925d6c0dd9ab48
Grickham000 <guru.aalfa@gmail.com>
2024-04-07 09:15:56 -0600

    Adding the personalized API to get employee by id

--- todo/todo_api/urls.py
+++ todo/todo_api/urls.py
@@ -1,11 +1,11 @@
 # todo_api/urls.py
 from django.urls import path
-from .views import ListEmployeeAPIView, CreateEmployeeAPIView, UpdateEmployeeAPIView, DeleteEmployeeAPIView, RetrieveEmployeeAPIView
+from .views import ListEmployeeAPIView, CreateEmployeeAPIView, UpdateEmployeeAPIView, DeleteEmployeeAPIView, EmployeeDetail
 
 urlpatterns = [
     path('employees/', ListEmployeeAPIView.as_view(), name='employee-list'),
     path('employees/create/', CreateEmployeeAPIView.as_view(), name='employee-create'),
-    path('api/employees/<int:employee_id>/', RetrieveEmployeeAPIView.as_view(), name='retrieve-employee'),
+    path('employee/<int:pk>/', EmployeeDetail.as_view(), name='retrieve-employeeDetail'),
     path('employees/update/<int:pk>', UpdateEmployeeAPIView.as_view(), name='employee-update'),
     path('employees/<int:pk>/delete/', DeleteEmployeeAPIView.as_view(), name='employee-delete'),
 ]
--- todo/todo_api/views.py
+++ todo/todo_api/views.py
@@ -1,5 +1,12 @@
 # todo_api/views.py
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView,RetrieveAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+
+#imports to create an specialized API
+from rest_framework.views import APIView # to create a specialized API
+from django.http import Http404 # to be able to return 404 status
+from rest_framework.response import Response # to be able to construct a rest response
+from rest_framework import status 
+
 from .models import Employee
 from .serializers import EmployeeSerializer
 
@@ -19,13 +26,17 @@ class DeleteEmployeeAPIView(DestroyAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
-class RetrieveEmployeeAPIView(RetrieveAPIView):
+class EmployeeDetail(APIView):
     """
-    API view to retrieve a single employee by ID.
+    Retrieve an employee instance.
     """
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+    def get_object(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            raise Http404
 
-    def get_object(self):
-        employee_id = self.kwargs.get('employee_id')
-        return get_object_or_404(Employee, id=employee_id)
+    def get(self, request, pk, format=None):
+        employee = self.get_object(pk)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data)
